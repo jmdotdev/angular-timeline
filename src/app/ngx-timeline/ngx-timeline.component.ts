8fc22ff99370ee2c39308ea9a3b85c3c8eeba0ef
JohnMwaniki-TheJitu <john.mwaniki@griffinglobaltech.com>
2024-05-17 13:17:22 +0300

    fix: fixed progress bar overflow

--- src/app/ngx-timeline/ngx-timeline.component.ts
+++ src/app/ngx-timeline/ngx-timeline.component.ts
@@ -8,7 +8,6 @@ interface TimelineEvent {
   description: string;
   icon: string;
   completed: boolean;
-  percentage?:number | null; // Track if the event is completed
 }
 @Component({
   selector: 'app-ngx-timeline',
@@ -23,22 +22,24 @@ export class NgxTimelineComponent implements OnInit {
   progressStage = 0;
   totalStages = 3;
   interval: any;
+  completePercentage = 0;
 
   timelineEvents: TimelineEvent[] = [
     { index:1,date: 'Jan 20', title: 'Onboarding', description: 'Preparation', icon: 'fas fa-rocket', completed: false },
-    { index:2,date: 'Feb 14', title: 'Created', description: 'Data checking', icon: 'fas fa-check-circle', completed: false },
+    { index:2,date: 'Feb 14', title: 'Axos', description: 'Data checking', icon: 'fas fa-check-circle', completed: false },
     { index:3,date: 'Mar 8', title: 'Client', description: 'Completed proof of concept', icon: 'fas fa-clipboard-check', completed: false },
     { index:4,date: 'Apr 30', title: 'Advisor', description: 'Signed Contract', icon: 'fas fa-file-signature', completed: false },
     { index:5,date: 'Jun 7', title: 'HCM', description: 'All information is migrated', icon: 'fas fa-database', completed: false },
     { index:6,date: 'Sep 16', title: 'Docupace', description: 'In Asia, Australia, Latin America', icon: 'fas fa-globe', completed: false },
     { index:7,date: 'Oct 20', title: 'Trillo', description: 'Signed Contract', icon: 'fas fa-file-signature', completed: false },
-    { index:8,date: 'Nov 30', title: 'Complete', description: 'Data checking', icon: 'fas fa-check-circle', completed: false },
+    { index:8,date: 'Nov 30', title: 'Complete', description: 'Process Done', icon: 'fas fa-check-circle', completed: false },
   ];
   activeEvent = this.timelineEvents[0];
   constructor() { }
 
   ngOnInit() {
     this.activeEvent = this.timelineEvents[this.activeIndex];
+    // this.setActiveEvent(this.activeIndex)
   }
 
   setActiveEvent(index: number): void {
@@ -49,7 +50,8 @@ export class NgxTimelineComponent implements OnInit {
   startProgress(): void {
     const stageDuration = 0.1 * 60 * 1000; // 1 seconds in milliseconds
     this.interval = setInterval(() => {
-      if (this.progressStage < this.totalStages) {
+      const percentageProgress = Math.ceil(((this.activeIndex + 1) / (this.timelineEvents.length -1))*100)
+      if ((this.progressStage < this.totalStages) && percentageProgress <= 100) {
         this.progressStage++;
         this.updateProgress();
       } else {
@@ -59,7 +61,6 @@ export class NgxTimelineComponent implements OnInit {
   }
 
   updateProgress(): void {
-    console.log("activeindex",this.activeIndex + 1)
     const percentagePerStage = 100 / (this.timelineEvents.length - 1) / 3;
     this.progress += percentagePerStage;
     // Calculate the starting point for the gradient
@@ -71,13 +72,13 @@ export class NgxTimelineComponent implements OnInit {
     let color: any;
     switch (this.progressStage) {
         case 1:
-            color = `linear-gradient(to right, green ${startPercentage}%, green ${endPercentage}%, green ${endPercentage}%)`;
+            color = `linear-gradient(to right, #35B288 ${startPercentage}%, #35B288 ${endPercentage}%, #35B288 ${endPercentage}%)`;
             break;
         case 2:
-            color = `linear-gradient(to right, green ${startPercentage}%, green ${endPercentage}%, orange ${endPercentage}%)`;
+            color = `linear-gradient(to right, #35B288 ${startPercentage}%, #35B288 ${endPercentage}%, #FF9900 ${endPercentage}%)`;
             break;
         case 3:
-            color = `linear-gradient(to right, green ${startPercentage}%, orange ${endPercentage}%,red ${endPercentage}%)`;
+            color = `linear-gradient(to right, #35B288 ${startPercentage}%, #FF9900 ${endPercentage}%,#FF3C3C ${endPercentage}%)`;
             break;
     }
     document.documentElement.style.setProperty('--progress-color', color);
@@ -85,12 +86,13 @@ export class NgxTimelineComponent implements OnInit {
 
 
   markAsComplete(index: number): void {
+    this.completePercentage = ((index + 1) / (this.timelineEvents.length -1) ) * 100;
+    this.completePercentage = Math.ceil(this.completePercentage)
     if (!(index + 1 == this.timelineEvents.length)){
        // this.startProgress();
-    this.setActiveEvent(index);
+    // this.setActiveEvent(index);
     // Mark the event as complete
     this.timelineEvents[index].completed = true;
-
     // Recalculate progress based on completed events
     const completedEvents = this.timelineEvents.filter(event => event.completed);
     const completedCount = completedEvents.length;
@@ -105,12 +107,11 @@ export class NgxTimelineComponent implements OnInit {
     }
 
     // Set the progress color to green
-    document.documentElement.style.setProperty('--progress-color', 'green');
+    document.documentElement.style.setProperty('--progress-color', '#35B288');
     this.progressStage = 0;
 
     }
     else{
-     alert("done");
      this.progress = 100; 
     }
   }
